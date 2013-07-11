f06d19d882a4cb1af97eb664c3d113aa8cffca59
Adam Wead <amsterdamos@gmail.com>
2013-07-11 13:57:47 -0400

    Ignoring bin and other conf files

--- bin/genkey.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-. ./setenv.sh
-
-$_EXECJAVA -cp $WMSAPP_HOME/bin/genkey.jar main.Main $1 $2 $3
--- bin/shutdown.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-. ./setenv.sh
-$_EXECJAVA -Dcom.wowza.wms.AppHome="$WMSAPP_HOME" -Dcom.wowza.wms.ConfigHome="$WMSCONFIG_HOME" -cp $WMSAPP_HOME/bin/wms-bootstrap.jar com.wowza.wms.bootstrap.Bootstrap stop
